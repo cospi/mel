a52d839b5692cd0ea1128aeafd5ad02ac02bf796
Aleksanteri Hirvonen <20629710+cospi@users.noreply.github.com>
2025-08-20 22:49:49 +0300

    Implement directory getter for Linux

--- mel.c
+++ mel.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 static char *mel_path(ssize_t *out_length)
 {
 	char *path = malloc(PATH_MAX);
@@ -29,16 +28,50 @@ static char *mel_path(ssize_t *out_length)
 	return path;
 }
 
+static ssize_t mel_get_separator_index(const char *path, ssize_t length)
+{
+	for (ssize_t i = length - 1; i >= 0; --i) {
+		if (path[i] == '/') {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+static char *mel_directory(void)
+{
+	ssize_t length;
+	char *directory = mel_path(&length);
+	if (directory == NULL) {
+		return NULL;
+	}
+
+	ssize_t separator_index = mel_get_separator_index(directory, length);
+	if (separator_index == -1) {
+		fputs("Getting separator index failed.\n", stderr);
+		free(directory);
+		return NULL;
+	}
+
+	directory[separator_index + 1] = '\0';
+	return directory;
+}
+
 int main(void)
 {
 	ssize_t length;
 	char *path = mel_path(&length);
-	if (path == NULL) {
-		return EXIT_FAILURE;
+	if (path != NULL) {
+		puts(path);
+		free(path);
 	}
 
-	puts(path);
+	char *directory = mel_directory();
+	if (directory != NULL) {
+		puts(directory);
+		free(directory);
+	}
 
-	free(path);
 	return EXIT_SUCCESS;
 }
